844bd1a4de2b398e7d9e0be1aff92ee3203e89e5
your_username <your_email>
2023-03-07 15:50:19 +0800

    2023年03月07日15:50:23

--- diy.sh
+++ diy.sh
@@ -2,14 +2,14 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #下载Adguardhome插件
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git openwrt/package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git openwrt/package/luci-app-adguardhome
 
 #下载关机插件
 git clone https://github.com/esirplayground/luci-app-poweroff openwrt/package/luci-app-poweroff
 
 #下载Smartdns插件
-git clone https://github.com/pymumu/openwrt-smartdns openwrt/package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git openwrt/package/luci-app-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns openwrt/package/smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git openwrt/package/luci-app-smartdns
 
 #下载Argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean/luci-theme-argon
