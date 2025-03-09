e6482cc1391bce213c5754fe214622631a454da7
John <john@163.com>
2025-03-09 18:22:31 +0800

    删除immortalwrt

--- diy-rax3000m-immortalwrt.sh
+++
@@ -1,5 +0,0 @@
-#修改默认IP地址
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-
-#下载Argon主题
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
