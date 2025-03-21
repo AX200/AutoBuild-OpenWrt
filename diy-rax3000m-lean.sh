d4c940b09f9afbb58e286ff622bdbb28f2de9180
John Doe <johndoe@example.com>
2025-03-21 20:43:41 +0800

    2025年03月21日20:43:43

--- diy-rax3000m-lean.sh
+++ diy-rax3000m-lean.sh
@@ -7,5 +7,5 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/f
 #下载Argon主题
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
-#编译测试版内核
+#编译测试版内核版本
 #sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/mediatek/Makefile
