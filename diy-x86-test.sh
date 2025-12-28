43c27eeb11d1a56d3aaa6136ca0ba19858bf2685
John Doe <johndoe@example.com>
2025-12-28 14:32:12 +0800

    2025年12月28日14:32:17

---
+++ diy-x86-test.sh
@@ -0,0 +1,18 @@
+#新版修改默认IP地址
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+#删除登录密码
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+
+#下载Adguardhome插件
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+
+#下载Argon主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+#添加Passwall源到feeds文件
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+
+#编译测试版内核
+#sed -i 's/KERNEL_PATCHVER:=6.12/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
